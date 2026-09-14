cad305811f36ac4dee2217e0afc76fdca79c6448
Nikhilyadav0001 <nikhilraoshab1@gmail.com>
2026-09-14 19:07:50 +0530

    JWT generation

--- auth-service/src/main/java/com/pm/authservice/dto/LoginRequestDTO.java
+++ auth-service/src/main/java/com/pm/authservice/dto/LoginRequestDTO.java
@@ -4,7 +4,7 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
-public class LoginRequestDto {
+public class LoginRequestDTO {
     @NotBlank(message = "Email is required")
     @Email(message = "Email should be a valid email address")
     private String email;
